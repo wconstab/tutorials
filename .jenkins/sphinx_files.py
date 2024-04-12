31f56771397cf35682419e3c817d3ee7115d1ba9
Catherine Lee <csl@fb.com>
2024-04-12 16:32:31 -0700

    update

--- .jenkins/sphinx_files.py
+++ .jenkins/sphinx_files.py
@@ -1,6 +1,7 @@
+import shutil
 import subprocess
 import os
-
+import glob
 
 def main() -> None:
     files_to_run = os.environ["FILES_TO_RUN"]
@@ -8,8 +9,12 @@ def main() -> None:
     for file in files_to_run.split(" "):
         print(f"Running {file}")
         env["RUNTHIS"] = file
-        subprocess.check_output(["make", "html"], env=env)
-
+        subprocess.check_output(["make", "html", f"BUILDDIR=_build/{file}"], env=env)
+        files = glob.glob(f"_build/{file}/**", recursive=True)
+        for gen_file in files:
+            if file in str(gen_file):
+                rel_path = os.path.relpath(gen_file, f"_build/{file}")
+                shutil.copy(gen_file, f"_build/{rel_path}")
 
 if __name__ == "__main__":
     main()
